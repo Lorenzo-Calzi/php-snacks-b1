601772ef3bf07eed1fda91ef6323005f42ae12d8
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-22 15:59:39 +0200

    Aggiunto Esercizio 3

---
+++ snack_1/index.php
@@ -0,0 +1,34 @@
+<?php
+    $calendar = [
+        'squad_1' => 'Olimpia Milano',
+        'squad_2' => 'Cantù',
+        'punteggio_1' => '10',
+        'punteggio_2' => '20',
+    ];
+    var_dump($calendar);
+
+
+    for( $i=0; $i < count($calendar); $i++){
+        echo '<p>';
+        echo $calendar;
+        echo '</p>';
+    }
+
+    /* 'giornata_1' => ['Olimpia Milano', 'Cantu'],   */
+    
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ snack_6/index.php
@@ -0,0 +1,54 @@
+<?php
+ 
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+
+    /* var_dump($db['teachers']); */
+    var_dump($db);
+
+
+    foreach($db as $element){
+        foreach ( $element as $key => $value ) {
+            echo "<dt>$key</dt><dd>$value</dd>";
+
+          }
+    }
+
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+
+</body>
+</html>
