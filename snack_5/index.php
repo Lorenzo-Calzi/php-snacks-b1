46a32d7a02cc022cb9e83ded5c18d3e17f1aa10f
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-22 15:28:46 +0200

    Aggiunto snack_5

---
+++ snack_5/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $paragraph = 
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Deserunt magnam nihil placeat, quo unde ipsum tempore sequi possimus dolores quia ad ab perferendis praesentium officia voluptate laudantium necessitatibus consequuntur voluptas quaerat veniam numquam enim fuga. 
+        Odio commodi eveniet perferendis veritatis, dolorum dicta. 
+        Quod nesciunt rem, libero nihil tempora nisi quaerat.';
+
+    var_dump(explode(".", $paragraph));
+
+?>
