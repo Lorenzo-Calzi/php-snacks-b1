eed3990376557a96e090a6ea327379302e2c5e8b
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-22 17:06:00 +0200

    Corretto snack_6

--- snack_6/index.php
+++ snack_6/index.php
@@ -24,19 +24,39 @@
     ];
 
 
-    /* var_dump($db['teachers']); */
-    var_dump($db);
+    /* var_dump($db); */
 
+    foreach($db as $type => $elements){
+        if($type === 'teachers'){
+            echo '<div style="background-color: grey; width: 50%; margin: auto; text-align:center; font-size:20px; color:red; font-size:30px">';
+                echo '<p>';
+                    echo $type;
+                echo '</p>';
+            echo '<div>';
 
-    foreach($db as $element){
-        foreach ( $element as $key => $value ) {
-            echo "<dt>$key</dt><dd>$value</dd>";
+        } else {
+            echo '<div style="background-color: green; text-align:center; font-size:20px; color:red">';
+                echo '<p>';
+                    echo $type;
+                echo '</p>';
+            echo '<div>';
+        }
+        
+        foreach ( $elements as $element) {
 
+            foreach( $element as $value){
+                echo '<div style="color:white; font-size:25px">';
+                    echo '<br>';
+                        echo $value;
+                    echo '</br>';
+                echo '<div>';
+            }
           }
     }
 
  
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -44,11 +64,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack 6</title>
 </head>
 <body>
     
-    
+    <div style="background-color: red; color: white; font-size:30px" ></div>
 
 </body>
 </html>
